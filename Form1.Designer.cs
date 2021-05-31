31d9909b763e4d68fd9e7a6de74f11af6e7a8ed8
1914795885 <1914795885@qq.com>
2021-05-31 21:54:12 +0800

    增加注释，添加图片文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace CSharp_悬浮球
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labGIF = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labGIF
+            // 
+            this.labGIF.Image = global::CSharp_悬浮球.Properties.Resources.正常状态;
+            this.labGIF.Location = new System.Drawing.Point(0, 0);
+            this.labGIF.Name = "labGIF";
+            this.labGIF.Size = new System.Drawing.Size(90, 120);
+            this.labGIF.TabIndex = 0;
+            this.labGIF.MouseDown += new System.Windows.Forms.MouseEventHandler(this.labGIF_MouseDown);
+            this.labGIF.MouseEnter += new System.EventHandler(this.labGIF_MouseEnter);
+            this.labGIF.MouseLeave += new System.EventHandler(this.labGIF_MouseLeave);
+            this.labGIF.MouseMove += new System.Windows.Forms.MouseEventHandler(this.labGIF_MouseMove);
+            this.labGIF.MouseUp += new System.Windows.Forms.MouseEventHandler(this.labGIF_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(92, 120);
+            this.ControlBox = false;
+            this.Controls.Add(this.labGIF);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.SystemColors.Control;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labGIF;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_悬浮球
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 当鼠标划过 更换gif
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void labGIF_MouseEnter(object sender, EventArgs e)
+        {
+            labGIF.Image = Properties.Resources.鼠标滑过;
+        }
+
+        /// <summary>
+        /// 当鼠标离开控件 更换GIF
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void labGIF_MouseLeave(object sender, EventArgs e)
+        {
+            labGIF.Image = Properties.Resources.正常状态;
+        }
+
+        /// <summary>
+        /// 记录鼠标按下坐标
+        /// </summary>
+        Point startPoint = new Point();
+        /// <summary>
+        /// 记录鼠标左键是否已经按下
+        /// </summary>
+        bool isDown = false;
+        /// <summary>
+        /// 鼠标按下回调函数
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void labGIF_MouseDown(object sender, MouseEventArgs e)
+        {
+            //左键按下 记住按下坐标
+            if(e.Button is MouseButtons.Left)
+            {
+                startPoint = e.Location;
+                isDown = true;
+            }
+            
+        }
+        /// <summary>
+        /// 鼠标弹起回调函数
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void labGIF_MouseUp(object sender, MouseEventArgs e)
+        {
+            //左键弹起 清除按下标志位
+            if (e.Button is MouseButtons.Left)
+            {
+                isDown = false;
+            }
+        }
+        /// <summary>
+        /// 鼠标在控件上移动回调函数
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void labGIF_MouseMove(object sender, MouseEventArgs e)
+        {
+            //左键移动 修改窗体坐标
+            if (e.Button is MouseButtons.Left && isDown)
+            {
+                this.Location = this.PointToScreen(new Point(e.X - startPoint.X, e.Y - startPoint.Y));
+            }
+        }
+    }
+}
